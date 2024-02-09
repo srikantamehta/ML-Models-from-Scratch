3dbc9ae72e7015bf675080af3dc33fd8553144ed
srikantamehta <srikanta.mehta@gmail.com>
2024-02-09 09:48:43 -0500

    Improvements to knn class

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from src.evaluation import Evaluation
 from models.knn import KNN
 from models.null_model import NullModelClassification, NullModelRegression
 from data_configs.configs import *
+import statistics
 
 
-config = albalone_config
+config = house_votes_84_config
 
 data_processor = DataProcessor(config=config)
 cross_validator = CrossValidation(config=config)
@@ -16,15 +17,9 @@ regression_nullmodel = NullModelRegression(config=config)
 
 raw_data = data_processor.load_data()
 
-# print(raw_data)
-
 data_1 = data_processor.impute_missing_values(raw_data)
 
-# print(data_1)
-
 data_2 = data_processor.encode_nominal_features(data_1)
-
-# print(data_2)
 
 data_3 = data_processor.encode_ordinal_features(data_2)
 
@@ -32,13 +27,10 @@ data_train, data_test = cross_validator.random_partition(data_3)
 
 knn_model = KNN(config, data_test, data_train)
 
-data_test_drop = data_test.drop(config['target_column'], axis=1)
+test_predictions = knn_model.knn_classifier(data_test, 10)
 
-distances = knn_model.k_nearest_neighbors(data_test_drop.iloc[1], 2)
+print(test_predictions)
 
-print(distances)
+eval_scores = Evaluation().zero_one_loss(test_predictions[config['target_column']],test_predictions['Predicted Class'])
 
-
-
-
-
+print(eval_scores)
--- models/knn.py
+++ models/knn.py
@@ -1,40 +1,87 @@
-import pandas as pd
-import numpy as np
-
 class KNN:
+    """
+    An implementation of the K-Nearest Neighbors algorithm for classification.
+
+    Attributes:
+        config (dict): Configuration settings, including the name of the target column.
+        test_set (DataFrame): The test dataset.
+        train_set (DataFrame): The training dataset.
+        train_set_drop (DataFrame): The training dataset with the target column dropped.
+        length_train_set_drop (int): The number of instances in the training dataset after dropping the target column.
+    """
 
     def __init__(self, config, test_set, train_set):
+        """
+        Initializes the KNN classifier with the given configuration, test set, and training set.
+
+        Parameters:
+            config (dict): Configuration settings, including the name of the target column.
+            test_set (DataFrame): The test dataset.
+            train_set (DataFrame): The training dataset.
+        """
         self.config = config
         self.test_set = test_set
         self.train_set = train_set
+        self.train_set_drop = self.train_set.drop(columns=[self.config['target_column']])
+        self.length_train_set_drop = len(self.train_set_drop)
 
     def calc_euclidian_distance(self, x1, x2):
-        distance = 0
-        for i in range(len(x1)):
-            distance += (x1[i]-x2[i]) ** 2
+        """
+        Calculates the Euclidean distance between two points.
+
+        Parameters:
+            x1: The first point.
+            x2: The second point.
+
+        Returns:
+            float: The Euclidean distance between the two points.
+        """
+        distance = sum((x1_val - x2_val) ** 2 for x1_val, x2_val in zip(x1, x2))
         return distance ** 0.5
 
     def k_nearest_neighbors(self, test_point, k):
-        
-        train = self.train_set.drop(self.config['target_column'], axis=1)
+        """
+        Finds the k nearest neighbors of a given test point.
 
-        distances = []
-        for index in range(len(train)):
-            distances.append(self.calc_euclidian_distance(test_point, train.iloc[index]))
+        Parameters:
+            test_point: The test point.
+            k (int): The number of nearest neighbors to find.
 
-        sorted_distances = sorted(distances)
-        k_nearest_neighbors = sorted_distances[:k]
-        
+        Returns:
+            list of tuples: A list of tuples containing the distance to the test point and the class of each of the k nearest neighbors.
+        """
+        distances = [(self.calc_euclidian_distance(test_point, self.train_set_drop.iloc[index]), 
+                      self.train_set.iloc[index][self.config['target_column']])
+                     for index in range(self.length_train_set_drop)]
+        k_nearest_neighbors = sorted(distances, key=lambda x: x[0])[:k]
         return k_nearest_neighbors
 
     def knn_classifier(self, test_set, k):
+        """
+        Classifies each instance in the test set based on the k nearest neighbors algorithm.
+
+        Parameters:
+            test_set (DataFrame): The test dataset.
+            k (int): The number of nearest neighbors to use for classification.
+
+        Returns:
+            DataFrame: The test set with an additional column for the predicted class.
+        """
+        test_set_features = test_set.drop(columns=[self.config['target_column']])
+        predictions = []
+
+        for index, row in test_set_features.iterrows():
+            neighbors = self.k_nearest_neighbors(row, k)
+            classes = [neighbor[1] for neighbor in neighbors]
+            predicted_class = max(set(classes), key=classes.count)  
+            predictions.append(predicted_class)
         
-        test_set_drop_class = test_set.drop(self.config['target_column'], axis=1)
-
-        for index in range(len(test_set_drop_class)):
-            k_nearest_neighbors = self.k_nearest_neighbors(test_set_drop_class.iloc[index], k)            
-
-        pass
+        test_set_with_predictions = test_set.copy()
+        test_set_with_predictions['Predicted Class'] = predictions
+        return test_set_with_predictions
 
     def knn_regression(self):
+        """
+        Placeholder for KNN regression implementation.
+        """
         pass
